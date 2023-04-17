e1f79d0e0967bb544c8d69da06674c75b8a40a80
LaurenRMorgan <87435641+LaurenRMorgan@users.noreply.github.com>
2023-04-17 20:57:02 +0100

    added new fonts and UI sprites, modified game over behaviour

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+
     public Vector3 targetLocation;
+    public int enemiesRemaining;
+    public TextMeshProUGUI enemyText;
+
+    public void Start(){
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        enemiesRemaining = enemies.Length;
+        enemyText.text = "Enemies Remaining " + enemiesRemaining.ToString();
+
+    }
+
+    public void Update(){
+
+GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+enemiesRemaining = enemies.Length;
+enemyText.text = "Enemies Remaining " + enemiesRemaining.ToString();
+
+    }
+
+
+
 
     public void MoveAllAIControllersToTarget()
     {
